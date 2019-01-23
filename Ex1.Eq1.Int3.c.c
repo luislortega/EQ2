868698b08a3f4f9e9d426f421a1197a42f26b042
Jorge Pinto <jorgeepl18@gmail.com>
2019-01-23 08:39:21 -0600

    Ya agregué mi parte

---
+++ Ex1.Eq1.Int3.c.c
@@ -0,0 +1,18 @@
+/*
+Autor:Jorge Pinto 23/01/2019
+Entradas:
+Salidas:
+Procedimiento general:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+
+	/*Salidas*/
+	return 0;
+}
+
