f2d1820a25d9d7cf8e974ffbe5c5a49bb194d006
Rodrigo <rodrigoh.gongora@gmail.com>
2019-01-23 08:37:11 -0600

    mi parte

---
+++ Ex1.Eq2.Int1.c
@@ -0,0 +1,18 @@
+/*
+Autor:Rodrigo Hernández 23/Enero/19
+Entradas:
+Salidas:
+Procedimiento general:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+
+	/*Salidas*/
+	return 0;
+}
+
